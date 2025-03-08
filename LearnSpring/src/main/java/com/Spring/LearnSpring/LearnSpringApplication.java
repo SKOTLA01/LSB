79096bfa03cb67a9c54152ebbb5604a9cb345822
saiteja-107 <kst@SAIs-MacBook-Air.local>
2025-03-09 00:04:51 +0530

    Spring application

---
+++ LearnSpring/src/main/java/com/Spring/LearnSpring/LearnSpringApplication.java
@@ -0,0 +1,15 @@
+package com.Spring.LearnSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LearnSpringApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
