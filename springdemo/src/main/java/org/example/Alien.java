b22435a18151066373cd63312e8344ff37dff093
saiteja-107 <kst@SAIs-MacBook-Air.local>
2025-03-09 01:27:14 +0530

    Spring added configurartion

--- springdemo/src/main/java/org/example/Alien.java
+++ springdemo/src/main/java/org/example/Alien.java
@@ -1,7 +1,30 @@
 package org.example;
 
 public class Alien {
+    private int age;
+    private  Laptop lap;
+    public int getAge() {
+        return age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Alien(){
+        System.out.println("Object created");
+    }
     public  void coding(){
         System.out.println("Coding");
+        lap.compile();;
     }
+
 }
